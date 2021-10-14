d353cefef78dfb45e8f891df9499fd62f50a119e
Baráth Bálint <balintbarath@gmail.com>
2021-10-14 18:11:57 +0200

    service names from config, periodic check

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -44,6 +45,8 @@ type Watched struct {
 	Pods    *v1.PodList
 }
 
+//type targets []Watched
+
 func main() {
 	// creates the in-cluster config
 	config, err := rest.InClusterConfig()
@@ -58,64 +61,97 @@ func main() {
 
 	//get config
 	service_filter := os.Getenv("SERVICE_FILTER")
-	fmt.Println(service_filter)
+	check_period_string := os.Getenv("CHECK_PERIOD")
+
+	check_period, err := strconv.Atoi(check_period_string)
+	if err != nil {
+		check_period = 5
+		fmt.Println("ERROR converting string to int for check period!")
+		//panic(err.Error())
+	}
+
+	serviceNames := strings.SplitN(service_filter, ",", -1)
+
+	currentTargets := make([]Watched, 0)
+
+	currentTargets = refreshTargets(clientset, serviceNames, currentTargets)
+
+	for _, item := range currentTargets {
+		fmt.Println("Found service name: " + item.Service.Name + ", pods: " + item.Pods.Items[0].Name)
+	}
 
 	for {
-		// get pods in all the namespaces by omitting namespace
-		// Or specify namespace to get pods in particular namespace
-		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+		for _, item := range currentTargets {
+			resp, err := http.Get("http://" + item.Pods.Items[0].Status.PodIP + ":8080/ping")
+			if err != nil {
+				panic(err.Error())
+			}
+			defer resp.Body.Close()
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				panic(err.Error())
+			}
 
-		//get services
-		services, err := clientset.CoreV1().Services("").List(context.TODO(), metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
+			fmt.Printf("Response: %s \n", body)
 		}
-		fmt.Printf("There are %d services in the cluster\n", len(services.Items))
 
-		var watched_service *v1.Service
-		for _, s := range services.Items {
-			if strings.HasPrefix(s.Name, "docker-go-ping") {
-				watched_service = s.DeepCopy()
-				fmt.Printf("Service name: %s \n", watched_service.Name)
+		time.Sleep(time.Duration(check_period) * time.Second)
+	}
+}
+
+func refreshTargets(clientset *kubernetes.Clientset, serviceNames []string, receiverTargets []Watched) []Watched {
+	//get services
+	services_all, err := clientset.CoreV1().Services("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("There are %d services in the cluster\n", len(services_all.Items))
+
+	for _, name := range serviceNames {
+		var found bool = false
+		for _, service := range services_all.Items {
+			//get service, pods based on service name
+			if strings.HasPrefix(service.Name, name) {
+				found = true
+				var watched Watched
+				watched.Service = service
+
+				//get pods for service in default namespace
+				servicepods, err := getPodsForSvc(&service, "default", clientset)
+				if err != nil {
+					panic(err.Error())
+				}
+				watched.Pods = servicepods
+				receiverTargets = append(receiverTargets, watched)
+				fmt.Println("New target added: " + watched.Service.Name)
 			}
 		}
 
-		for _, port := range watched_service.Spec.Ports {
-			fmt.Printf("Service port and nodeport: %d, %d \n", port.Port, port.NodePort)
+		if !found {
+			fmt.Println("Service not found for configured service name: " + name)
 		}
-
-		servicepods, err := getPodsForSvc(watched_service, "default", clientset)
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("Pod connected to service and ip: %s %s \n", servicepods.Items[0].Name, servicepods.Items[0].Status.PodIP)
-
-		resp, err := http.Get("http://" + servicepods.Items[0].Status.PodIP + ":8080/ping")
-		if err != nil {
-			panic(err.Error())
-		}
-		defer resp.Body.Close()
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			panic(err.Error())
-		}
-
-		fmt.Printf("Response: %s \n", body)
-
-		time.Sleep(5 * time.Second)
 	}
+
+	return receiverTargets
+
 }
+
+/*func (services *v1.Service) getService(name string) {
+	var watched_service *v1.Service
+	for _, s := range services.Items {
+		if strings.HasPrefix(s.Name, "docker-go-ping") {
+			watched_service = s.DeepCopy()
+			fmt.Printf("Service name: %s \n", watched_service.Name)
+		}
+	}
+}*/
 
 func getPodsForSvc(svc *v1.Service, namespace string, k8sClient *kubernetes.Clientset) (*v1.PodList, error) {
 	set := labels.Set(svc.Spec.Selector)
 	listOptions := metav1.ListOptions{LabelSelector: set.AsSelector().String()}
 	pods, err := k8sClient.CoreV1().Pods(namespace).List(context.TODO(), listOptions)
-	for _, pod := range pods.Items {
+	/*for _, pod := range pods.Items {
 		fmt.Fprintf(os.Stdout, "pod name: %v\n", pod.Name)
-	}
+	}*/
 	return pods, err
 }
